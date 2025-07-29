f094a10438d877f5f6e4fd86737e0fde11afcecd
Rodríguez Giovanni <grodriguez@bcbcasadebolsa.com>
2025-07-29 16:09:38 -0600

    [master] [added] [TrustRequest element added]

---
+++ src/main/java/com/bcb/trust/front/modules/request/controller/api/RequestRequestController.java
@@ -0,0 +1,20 @@
+package com.bcb.trust.front.modules.request.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/request")
+public class RequestRequestController {
+
+    @PostMapping("/create")
+    public String saveRequest(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+}
