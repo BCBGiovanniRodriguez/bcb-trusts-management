7fe0e3511815d4b98a89efe28efdd85ae231dc43
Rodríguez Giovanni <grodriguez@bcbcasadebolsa.com>
2026-04-12 15:21:52 -0600

    [legacy20260329] [fixed] [@NonNull annotations added]

--- src/main/java/com/bcb/trust/front/config/CorsConfig.java
+++ src/main/java/com/bcb/trust/front/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.bcb.trust.front.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,7 +12,7 @@ public class CorsConfig  implements WebMvcConfigurer {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins("http://localhost:10101", "http://localhost:9000", "http://localhost:9001", "http://localhost:9002", "http://10.20.50.132:10101", "http://10.20.50.132:9000", "http://10.20.50.132:9001", "http://10.20.50.132:9002")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
--- src/main/java/com/bcb/trust/front/modules/catalog/controller/CatalogConsultantController.java
+++ src/main/java/com/bcb/trust/front/modules/catalog/controller/CatalogConsultantController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,7 +30,7 @@ public class CatalogConsultantController {
 
     @Operation(summary = "Crear o actualizar un consultor", description = "Permite crear o actualizar un consultor en el sistema. Si se proporciona un ID existente, se actualizará el registro correspondiente; de lo contrario, se creará un nuevo registro.")
     @PostMapping("/consultant")
-    public String create(@RequestBody CatalogConsultantEntity consultant) throws JsonProcessingException {
+    public String create(@NonNull @RequestBody CatalogConsultantEntity consultant) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         String jsonResponse = null;
